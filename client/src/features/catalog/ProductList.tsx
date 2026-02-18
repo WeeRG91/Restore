5919b1969fa3060a82adc437b42373b696651122
WeeRG91 <126579703+WeeRG91@users.noreply.github.com>
2026-02-18 15:30:12 +0100

    Creating the React project

---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material"
+import type { Product } from "../../app/models/product"
+import ProductCard from "./ProductCard"
+
+type Props = {
+    products: Product[]
+}
+
+function ProductList({products}: Props) {
+  return (
+    <Box sx={{display: "flex", flexWrap: "wrap", gap: 3, justifyContent: 'center'}}>
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
+    </Box>
+  )
+}
+
+export default ProductList
